25be851eb1a36e96c9161587043eb56717d6b0e2
Samuel Meira <samuelleandmeira@hotmail.com>
2019-10-22 16:15:32 -0300

    added genserver in start link

--- lib/elastic_apm/agent.ex
+++ lib/elastic_apm/agent.ex
@@ -2,6 +2,8 @@ defmodule ElasticAPM.Agent do
   use GenServer
   
   def start_link(module) do
+    options = []
+    GenServer.start_link(__MODULE__, options, name: __MODULE__)
   end
 
   @impl GenServer
@@ -10,17 +12,19 @@ defmodule ElasticAPM.Agent do
   end
 
   def start() do
+    require IEx; IEx.pry()
     GenServer.cast(__MODULE__, :setup)
   end
 
   @impl GenServer
   def handle_cast(:setup, state) do
+    require IEx; IEx.pry()
     #TODO create the setup function like ruby agent.
     initialize()
   end
 
   @impl GenServer
-  def handle_cast(:send, _from, state) do
+  def handle_call(:send, _from, state) do
   end
 
   def initialize do
