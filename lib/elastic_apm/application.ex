df9e7b83cf051d1ac2b802a2a47c8d691dd2db60
Samuel Meira <samuelleandmeira@hotmail.com>
2019-10-22 09:45:10 -0300

    fixed start param name

--- lib/elastic_apm/application.ex
+++ lib/elastic_apm/application.ex
@@ -1,7 +1,8 @@
 defmodule ElasticApm.Application do
   use Application
   
-  def start(_fitas, _args) do
+  def start(_type, _args) do
+    require IEx; IEx.pry()
     children = []
     opts = [strategy: :one_for_all, name: ElasticApm.Supervisor]
     Supervisor.start_link(children, opts)
