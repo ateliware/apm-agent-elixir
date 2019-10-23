cb115454efb16c3eeda744c3228e96c797686d3d
Samuel Meira <samuelleandmeira@hotmail.com>
2019-10-23 17:30:03 -0300

    added telemetry in deps

--- mix.exs
+++ mix.exs
@@ -24,7 +24,8 @@ defmodule ElasticApm.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.18", only: :dev}
+      {:ex_doc, "~> 0.18", only: :dev},
+      {:telemetry, "~> 0.4.0 or ~> 0.3.0", optional: true}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
